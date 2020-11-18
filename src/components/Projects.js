856ea9c176e78cc481ae56463ae605dcfaee1ac7
BbeatrizSI <bbeatriz@gmail.com>
2020-11-18 22:09:23 +0100

    trying to make slide of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,21 +5,25 @@ import '../stylesheets/layout/Projects.scss';
 
 const Projects = ({data}) => {
     return (
-        <section className="projects" id="projects">
-            <h2 className="projects_title">Algo de lo que he estado haciendo</h2>
+        <section className="projects">
+          <div className="transparent_div" id="projects"></div>
+          <h2 className="projects_title">Algo de lo que he estado haciendo...</h2>
+          <div className="projects_container">
             {data.map(
-          ({ id, title, description, githubLink, webLink, img, skills }) => (
-            <Project
-              key={id}
-              title={title}
-              description={description}
-              githubLink={githubLink}
-              webLink={webLink}
-              img={img}
-              skills={skills}
-            />
-          )
-        )}
+              ({ id, title, description, githubLink, webLink, img, skills }) => (
+                <Project
+                  key={id}
+                  title={title}
+                  description={description}
+                  githubLink={githubLink}
+                  webLink={webLink}
+                  img={img}
+                   skills={skills}
+                />
+               )
+            )}
+          </div>
+          <p className="github_description">Para ver más te invito a pasarte por mi perfil de GitHub <a href="https://github.com/BbeatrizSI" title="Vamos a ver esos repositorios!"><i class="fab fa-github github_icon"></i></a></p>
         </section>
     );
 };

d5296a69237f70732de49aa748bba5747edaf254
BbeatrizSI <bbeatriz@gmail.com>
2020-11-21 21:41:41 +0100

    add arrow down in hero with its animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
             <h1 className="hero_title">Hola!  Soy <span className="hero_title-name">Bárbara B. Sacristán</span></h1>
             <h2 className="hero_description">Desarrolladora Front-End Jr.</h2>
             <p>Sigue bajando o ataja por los links para saber un poco más de mí!</p>
-            <i class="fas fa-chevron-down"></i>
+            <a className="arrow_down" href="#projects" title="Sigue avanzando!"><i class="fas fa-chevron-down"></i></a>
         </div>
     );
 };

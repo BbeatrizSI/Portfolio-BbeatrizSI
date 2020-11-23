cafd3f287a3c9cb601e620768e6c5271b9c76b33
BbeatrizSI <bbeatriz@gmail.com>
2020-11-23 21:14:12 +0100

    fix little bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,16 +10,17 @@ const Header = ({menuVisibility, setMenuVisibility}) => {
 
 
     return (
-        <nav className="nav_container">
-            <ul className="nav">
-                <li className="menu_icon js-menu" onClick={handleMenu}>{menuVisibility === false ? <i class="fas fa-bars"></i> : <i class="far fas fa-times-circle"></i>}</li>
-                <li className="projects_icon"><a href="#projects" title="Vamos a ver qué he hecho hasta ahora"><i class="fas fa-briefcase"></i></a></li>
-                <li className="me_icon"><a href="#aboutme" title="Quieres conocerme?"><i class="fas fa-user-alt"></i></a></li>
-                {/* <li><a href="#contact" title="Déjame un comentario">Sugerencias?</a></li> */}
-            </ul>
-            {menuVisibility === true ? <Menu /> : ''}
-        </nav>
-        
+        <header className="header">
+            <nav className="nav_container">
+                <ul className="nav">
+                    <li className="menu_icon js-menu" onClick={handleMenu}>{menuVisibility === false ? <i class="fas fa-bars"></i> : <i class="far fas fa-times-circle"></i>}</li>
+                    <li className="projects_icon"><a href="#projects" title="Vamos a ver qué he hecho hasta ahora"><i class="fas fa-briefcase"></i></a></li>
+                    <li className="me_icon"><a href="#aboutme" title="Quieres conocerme?"><i class="fas fa-user-alt"></i></a></li>
+                    {/* <li><a href="#contact" title="Déjame un comentario">Sugerencias?</a></li> */}
+                </ul>
+                {menuVisibility === true ? <Menu /> : ''}
+            </nav>
+        </header>
     );
 };
 

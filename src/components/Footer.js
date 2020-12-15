2fd6b59111e25507dd5216af2b3b59e44f91ac46
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-15 20:49:10 +0100

    run docs adding favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../stylesheets/layout/Footer.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 
 const Footer = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000});
+    }, []);
     return (
         <footer className="footer">
             <a href="mailto:bbeatriz@gmail.com" title="Escríbeme"><i className="fas fa-at"></i></a>
-            <div className="smalls_container">
+            <div className="smalls_container" data-aos="zoom-in">
                 <small className="small">Made with <i className="fas fa-heart"></i></small>
                 <small className="small">by BbeatrizSI ©2020</small>
             </div>

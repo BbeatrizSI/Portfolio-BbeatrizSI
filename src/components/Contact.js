19eb34ff816084dbbc6ba6fda38dcea9bcbae6d1
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-16 19:45:19 +0100

    form working with formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ const Contact = () => {
     return (
         <section className="contact">
             <h2 className="contact_title">Tienes alguna pregunta <span className="contact_title-secondary">o quieres trabajar conmigo?</span></h2>
-            <form className="contact_form" action="" name="telefonica">
-                <input type="text" name="name" placeholder="Nombre"/>
-                <input type="email" name="email" placeholder="e-mail"/>
-                <input type="text" name="message" placeholder="Tu mensaje"/>
+            <form className="contact_form" action="https://formspree.io/f/xzbkjqky" method="POST">
+                <input type="text" name="name" placeholder="Nombre" required/>
+                <input type="email" name="email" placeholder="e-mail" required/>
+                <input type="text" name="message" placeholder="Tu mensaje" required/>
                 <button type="submit" className="contact_form-btn">Enviar</button>
             </form>
         </section>

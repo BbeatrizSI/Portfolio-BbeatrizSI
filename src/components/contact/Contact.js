44c5888df016fb62f5251e82d750e74333468a69
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-21 15:11:51 +0100

    form correctly working

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Form from '../form/Form';
-import SuccessSubmit from '../success/SuccessSubmit';
-import ErrorSubmit from '../errorsubmit/ErrorSubmit';
 import './Contact.scss';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
-const Contact = ({ history }) => {
+const Contact = () => {
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
   const [stateForm, setStateForm] = useState('');
 
-  const resultFormSubmit = () => {
-    if (stateForm === 'SUCCESS') {
-      <SuccessSubmit />;
-    } else if (stateForm === 'ERROR') {
-      <ErrorSubmit />;
+  const [values, setValues] = useState({ name: '', email: '', message: '' });
+
+  const handleInputChange = ({ target }) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+    if (target.value !== '') {
+      setStateForm('');
     }
-    console.log(stateForm);
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStateForm('SUCCESS');
+      } else {
+        setStateForm('ERROR');
+      }
+    };
+    xhr.send(data);
   };
 
   return (
@@ -31,18 +49,59 @@ const Contact = ({ history }) => {
           o quieres trabajar conmigo?
         </span>
       </h2>
-      <Switch>
-        <Route exact path='/'>
-          <Form
-            stateForm={stateForm}
-            setStateForm={setStateForm}
-            history={history}
-          />
-        </Route>
-        <Route exact path='/resultsubmit'>
-          {stateForm === 'SUCCESS' ? <SuccessSubmit /> : <ErrorSubmit />}
-        </Route>
-      </Switch>
+      <form
+        className='contact_form'
+        action='https://formspree.io/f/xzbkjqky'
+        method='POST'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type='text'
+          name='name'
+          placeholder='Nombre'
+          required
+          className='input'
+          value={values.values}
+          onChange={handleInputChange}
+        />
+        <input
+          type='email'
+          name='email'
+          placeholder='e-mail'
+          required
+          className='input'
+          value={values.values}
+          onChange={handleInputChange}
+        />
+        <textarea
+          name='message'
+          placeholder='Tu mensaje'
+          required
+          className='input'
+          value={values.values}
+          onChange={handleInputChange}
+        ></textarea>
+        <button type='submit' className='contact_form-btn'>
+          Enviar
+        </button>
+        {stateForm === '' && <div className='submit_response'></div>}
+        {stateForm === 'SUCCESS' && (
+          <div className='submit_response'>
+            <i className='fas fa-check'></i>
+            <p className='thankyou_text'>
+              Gracias! Muy pronto obtendrás respuesta!
+            </p>
+          </div>
+        )}
+        {stateForm === 'ERROR' && (
+          <div className='submit_response'>
+            <i className='fas fa-times'></i>
+            <p className='thankyou_text'>
+              Ooops! Algo salió mal, vuelve a intentarlo.
+            </p>
+          </div>
+        )}
+      </form>
     </section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './components/App';
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router>
-    <App history={history} />
-  </Router>,
+  <App />,
+
   document.getElementById('root')
 );

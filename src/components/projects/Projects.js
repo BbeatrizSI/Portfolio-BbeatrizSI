dc2054847306c801b9349c71b752b8b4d5958c63
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-12-22 16:45:49 +0100

    some tranmslations

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import Project from './Project';
 import './Projects.scss';
+import { useTranslation } from 'react-i18next';
 
+const Projects = ({ data }) => {
+  const [t] = useTranslation('global');
 
-const Projects = ({data}) => {
-    return (
-        <section className="projects">
-          <div className="transparent_div" id="projects"></div>
-          <h2 className="projects_title">Algo de lo que he estado haciendo...</h2>
-          <div className="projects_container">
-            {data.map(
-              ({ id, title, description, githubLink, webLink, img, skills }) => (
-                <Project
-                  key={id}
-                  title={title}
-                  description={description}
-                  githubLink={githubLink}
-                  webLink={webLink}
-                  img={img}
-                  skills={skills}
-                />
-               )
-            )}
-            <div className="blank_space"></div>
-          </div>
-          <div className="github_description">
-          <p>Para ver más, te invito a pasar por mi perfil de GitHub:</p> <a className="github_icon-link"href="https://github.com/BbeatrizSI" target="_blank" rel="noopener noreferrer" title="Enlace al perfil de GitHub"><i className="fab fa-github github_icon"></i></a>
-          </div>
-        </section>
-    );
+  return (
+    <section className='projects'>
+      <div className='transparent_div' id='projects'></div>
+      <h2 className='projects_title'>{t('projects.title')}</h2>
+      <div className='projects_container'>
+        {data.map(
+          ({ id, title, description, githubLink, webLink, img, skills }) => (
+            <Project
+              key={id}
+              title={title}
+              description={description}
+              githubLink={githubLink}
+              webLink={webLink}
+              img={img}
+              skills={skills}
+            />
+          )
+        )}
+        <div className='blank_space'></div>
+      </div>
+      <div className='github_description'>
+        <p>{t('projects.github')}</p>
+        <a
+          className='github_icon-link'
+          href='https://github.com/BbeatrizSI'
+          target='_blank'
+          rel='noopener noreferrer'
+          title='Enlace al perfil de GitHub'
+        >
+          <i className='fab fa-github github_icon'></i>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
--- src/data/dataProjects.js
+++ src/data/dataProjects.js
@@ -7,93 +7,92 @@ import pokemon from '../assets/images/pokemon.jpg';
 import series from '../assets/images/series.jpg';
 import tareas from '../assets/images/tareas.jpg';
 
-
-
 const dataProjects = [
-    {
-        id: 1,
-        title: 'Heroes App',
-        description:
-            'Directorio de héroes con buscador. Falso "login" para diferenciar rutas públicas y privadas',
-        githubLink: 'https://github.com/BbeatrizSI/Heroes-App',
-        webLink: 'https://bbeatrizsi.github.io/Heroes-App/',
-        img: heroes,
-        skills: ['HTML', 'Bootstrap', 'React', 'React-Router'],
-    },
-    {
-        id: 2,
-        title: 'Rick and Morty',
-        description:
-            'Buscador de personajes de la serie Rick y Morty. Dispone de filtros y puedes ver en detalle cada personaje.',
-        githubLink: 'https://github.com/BbeatrizSI/                 Rick-and-Morty-Characters',
-        webLink: 'https://bbeatrizsi.github.io/Rick-and-Morty-Characters/#/',
-        img: rick,
-        skills: ['HTML', 'SASS', 'React'],
-    },
-    {
-        id: 3,
-        title: 'Blackjack',
-        description:
-            'Sencillo juego de Blackjack hecho en JS Vanilla y maquetado con ayuda de Bootstrap. Ya sabéis, a por 21!',
-        githubLink: 'https://github.com/BbeatrizSI/BlackJack',
-        webLink: 'https://bbeatrizsi.github.io/BlackJack/',
-        img: black,
-        skills: ['HTML', 'Bootstrap', 'JS Vanilla'],
-    },
-    {
-        id: 4,
-        title: 'Card Generator',
-         description:
-            'Generador de tarjetas de visita, con uso de local storage. Una vez terminada se comparte en Twitter fácilmente.',
-        githubLink: 'https://github.com/BbeatrizSI/Maniac-Coders-Awesome-Profile-Cards',
-        webLink: 'https://bbeatrizsi.github.io/Maniac-Coders-Awesome-Profile-Cards/',
-         img: cards,
-        skills: ['HTML', 'CSS', 'SASS', 'JavaScript'],
-    },
-    {
-        id: 5,
-        title: 'Drum-Kit',
-        description:
-            'Sencillo juego de batería hecho con JavaScript Vanilla. Responsive y accionable tanto con teclas como pulsando!',
-        githubLink:
-            'https://github.com/BbeatrizSI/Drum-Kit',
-         webLink: 'https://bbeatrizsi.github.io/Drum-Kit/',
-        img: drum,
-         skills: ['HTML', 'CSS', 'JavaScript'],
-    },
-    {
-        id: 6,
-        title: 'Pokemon List',
-        description:
-            'Listado de Pokemons hecho con React, seleccionables y con un bonito (aunque sencillo) diseño.',
-        githubLink:
-            'https://github.com/BbeatrizSI/Pokemon-List',
-        webLink: 'https://bbeatrizsi.github.io/Pokemon-List/',
-        img: pokemon,
-        skills: ['HTML', 'SASS', 'React'],
-    },
-    {
-        id: 7,
-        title: 'Buscador de Series',
-        description:
-            'Buscador por nombre a través de API. Contiene un listado de favoritos y la info se almacena en el local storage.',
-        githubLink:
-            'https://github.com/BbeatrizSI/modulo-2-evaluacion-final-BbeatrizSI',
-        webLink: 'https://bbeatrizsi.github.io/modulo-2-evaluacion-final-BbeatrizSI/',
-        img: series,
-        skills: ['HTML', 'SASS', 'JavaScript'],
-    },
-    {
-        id: 8,
-        title: 'ToDo App',
-        description:
-            'Aplicación web para gestionar tareas pendientes. Tiene filtro para saber las que te quedan por realizar',
-        githubLink:
-            'https://github.com/BbeatrizSI/ToDo-App',
-        webLink: 'https://bbeatrizsi.github.io/ToDo-App/',
-        img: tareas,
-        skills: ['HTML', 'SASS', 'JavaScript', 'Webpack'],
-    },
-  ];
-  
-  export default dataProjects;
+  {
+    id: 1,
+    title: 'Heroes App',
+    description:
+      'Directorio de héroes con buscador. Falso "login" para diferenciar rutas públicas y privadas',
+    githubLink: 'https://github.com/BbeatrizSI/Heroes-App',
+    webLink: 'https://bbeatrizsi.github.io/Heroes-App/',
+    img: heroes,
+    skills: ['HTML', 'Bootstrap', 'React', 'React-Router'],
+  },
+  {
+    id: 2,
+    title: 'Rick and Morty',
+    description:
+      'Buscador de personajes de la serie Rick y Morty. Dispone de filtros y puedes ver en detalle cada personaje.',
+    githubLink:
+      'https://github.com/BbeatrizSI/                 Rick-and-Morty-Characters',
+    webLink: 'https://bbeatrizsi.github.io/Rick-and-Morty-Characters/#/',
+    img: rick,
+    skills: ['HTML', 'SASS', 'React'],
+  },
+  {
+    id: 3,
+    title: 'Blackjack',
+    description:
+      'Sencillo juego de Blackjack hecho en JS Vanilla y maquetado con ayuda de Bootstrap. Ya sabéis, a por 21!',
+    githubLink: 'https://github.com/BbeatrizSI/BlackJack',
+    webLink: 'https://bbeatrizsi.github.io/BlackJack/',
+    img: black,
+    skills: ['HTML', 'Bootstrap', 'JS Vanilla'],
+  },
+  {
+    id: 4,
+    title: 'Card Generator',
+    description:
+      'Generador de tarjetas de visita, con uso de local storage. Una vez terminada se comparte en Twitter fácilmente.',
+    githubLink:
+      'https://github.com/BbeatrizSI/Maniac-Coders-Awesome-Profile-Cards',
+    webLink:
+      'https://bbeatrizsi.github.io/Maniac-Coders-Awesome-Profile-Cards/',
+    img: cards,
+    skills: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+  },
+  {
+    id: 5,
+    title: 'Drum-Kit',
+    description:
+      'Sencillo juego de batería hecho con JavaScript Vanilla. Responsive y accionable tanto con teclas como pulsando!',
+    githubLink: 'https://github.com/BbeatrizSI/Drum-Kit',
+    webLink: 'https://bbeatrizsi.github.io/Drum-Kit/',
+    img: drum,
+    skills: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 6,
+    title: 'Pokemon List',
+    description:
+      'Listado de Pokemons hecho con React, seleccionables y con un bonito (aunque sencillo) diseño.',
+    githubLink: 'https://github.com/BbeatrizSI/Pokemon-List',
+    webLink: 'https://bbeatrizsi.github.io/Pokemon-List/',
+    img: pokemon,
+    skills: ['HTML', 'SASS', 'React'],
+  },
+  {
+    id: 7,
+    title: 'Buscador de Series',
+    description:
+      'Buscador por nombre a través de API. Contiene un listado de favoritos y la info se almacena en el local storage.',
+    githubLink:
+      'https://github.com/BbeatrizSI/modulo-2-evaluacion-final-BbeatrizSI',
+    webLink:
+      'https://bbeatrizsi.github.io/modulo-2-evaluacion-final-BbeatrizSI/',
+    img: series,
+    skills: ['HTML', 'SASS', 'JavaScript'],
+  },
+  {
+    id: 8,
+    title: 'ToDo App',
+    description:
+      'Aplicación web para gestionar tareas pendientes. Tiene filtro para saber las que te quedan por realizar',
+    githubLink: 'https://github.com/BbeatrizSI/ToDo-App',
+    webLink: 'https://bbeatrizsi.github.io/ToDo-App/',
+    img: tareas,
+    skills: ['HTML', 'SASS', 'JavaScript', 'Webpack'],
+  },
+];
+
+export default dataProjects;
